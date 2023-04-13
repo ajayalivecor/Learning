f55344ddadd3825828fe28bdd3315ee4430e3719
Ajayappledeveloper <asinginee956@alivecor.com>
2023-04-13 18:23:42 +0530

    Implemented App localisations.

--- Projects/AppLocalization/AppLocalization/ViewController.swift
+++ Projects/AppLocalization/AppLocalization/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(NSLocalizedString("Hello", comment: ""))
+        print(NSLocalizedString("Welcome", comment: ""))
     }
 
 
